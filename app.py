8d8de836becc6425da13bbd30cc35652d9fb9535
andrecendon <cendonandre@gmail.com>
2024-06-08 21:37:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
-from flask import render_template, request, flash, redirect, url_for, jsonify, abort
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-from flask_login import current_user, login_required,login_user, logout_user
-import atexit
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from datetime import datetime
 import math
 import random
+
 app = Flask(__name__)
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'your-secret-key'  # Clave secreta para la sesión
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # URL de la base de datos SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactiva el seguimiento de modificaciones
+db = SQLAlchemy(app)  # Inicializa SQLAlchemy con la app Flask
+login_manager = LoginManager(app)  # Inicializa el gestor de login
+bootstrap = Bootstrap(app)  # Inicializa Bootstrap (si estás usando esta extensión)
+
 
 class Sale(db.Model):
     OrderNumber = db.Column(db.Integer, primary_key=True)
