a5b3de649fd584b5eae1c0503f0de0375c14432a
andrecendon <cendonandre@gmail.com>
2024-06-08 14:03:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-from flask import Flask
-from flask import render_template, request, flash, redirect, url_for, jsonify, abort
-from flask_login import current_user, login_required,login_user, logout_user
-import atexit
-from datetime import datetime
-import math
-import random
-from functools import wraps
-import signal
+from flask import Flask, render_template
 app = Flask(__name__)
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-bootstrap = Bootstrap(app)
 
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    mensaje = None
-    
-    return render_template('index.html', mensaje=mensaje)
-
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register)
 def register():
-    
-    mensaje=None
-    if request.method == 'POST':
-       
-        
-       
-        return redirect(url_for('search'))
-        
-        
     return render_template('register.html')
