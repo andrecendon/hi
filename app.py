758998bfaef6cd5e562d2ec8118c9fd54b094255
andrecendon <cendonandre@gmail.com>
2024-06-08 13:55:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import render_template, request, flash, redirect, url_for, jsonify, abort
+from flask_login import current_user, login_required,login_user, logout_user
+import atexit
+from datetime import datetime
+import math
+import random
+from functools import wraps
+import signal
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    mensaje = None
+    
+    return render_template('index.html', mensaje=mensaje)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    
+    mensaje=None
+    if request.method == 'POST':
+       
+        
+       
+        return redirect(url_for('search'))
+        
+        
+    return render_template('register.html')
